4473affa38f796d2c0e3e91fa19addf327c2bcee
hirokishinoda <rss23843@nifty.com>
2017-02-18 21:20:31 +0900

    [add] add ball and ball_reflection
    新しくボールを追加して、反射の物理判定追加。

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import sys
 
 SCR_RECT = Rect(0,0,400,400)
 
-vx = 0
-
 #main関数
 def main():
     pygame.init()
@@ -19,19 +17,22 @@ def main():
     #スプライトグループを作成して登録
     all = pygame.sprite.RenderUpdates()
     Paddle.containers = all
-    #パドル作成
+    Ball.containers = all
+    #パドル作成(自動的にspriteに追加)
     paddle = Paddle()
+    #ボール作成(自動的にspriteに追加)
+    Ball(paddle)
 
     #ゲームループ
     clock = pygame.time.Clock()
     while True:
         clock.tick(60)
         screen.fill((0,0,0))
-
+        #spriteの更新
         all.update()
         all.draw(screen)
+        #画面の更新
         pygame.display.update()
-
         #イベント処理
         for event in pygame.event.get():
             if event.type == QUIT:#終了処理
@@ -66,6 +67,50 @@ class Paddle(pygame.sprite.Sprite):
         elif pressed_keys[K_LEFT]:
             self.rect.x -= 5;
         self.rect.clamp_ip(SCR_RECT)
+
+#ボールクラス
+class Ball(pygame.sprite.Sprite):
+    SPEED = 5
+    def __init__(self,paddle):
+        pygame.sprite.Sprite.__init__(self,self.containers)
+        self.image,self.rect = load_image("Ball.png")
+        self.move_x = self.move_y = 0
+        self.paddle = paddle
+        self.update = self.start
+    def start(self):
+        #パドルの中央に配置
+        self.rect.centerx = self.paddle.rect.centerx
+        self.rect.bottom = self.paddle.rect.top
+        #spacekey入力で移動開始
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_SPACE]:
+            self.move_x = self.SPEED
+            self.move_y = -self.SPEED
+            #updateをmoveに変更
+            self.update = self.move
+    def move(self):
+        #ボール移動
+        self.rect.centerx += self.move_x
+        self.rect.centery += self.move_y
+        #衝突の判定
+        #左の壁との衝突
+        if self.rect.left < SCR_RECT.left:
+            self.rect.left = SCR_RECT.left
+            self.move_x = -self.move_x
+        #右の壁との衝突
+        if self.rect.right > SCR_RECT.right:
+            self.rect.right = SCR_RECT.right
+            self.move_x = -self.move_x
+        #上の壁との衝突
+        if self.rect.top < SCR_RECT.top:
+            self.rect.top = SCR_RECT.top
+            self.move_y = -self.move_y
+        #パドルとの衝突
+        if self.rect.colliderect(self.paddle.rect) and self.move_y > 0:
+            self.move_y = -self.move_y
+        #画面の下側との衝突(落とした場合)
+        if self.rect.top > SCR_RECT.bottom:
+            self.update = self.start
 
 #起動時処理
 if __name__ == "__main__" :
